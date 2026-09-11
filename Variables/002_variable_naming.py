799560e40dda5c3627d85ccdd73383fdd6b55024
Shadman Shakib <shadmanshakib529@gmail.com>
2026-09-12 01:48:29 +0600

    Add files via upload

---
+++ Variables/002_variable_naming.py
@@ -0,0 +1,62 @@
+# Python Variable Naming Rules
+# 
+
+
+# Rule 1: Start with a letter or underscore
+name = "Shakib"
+_name = "Shakib"
+
+
+# Rule 2: Cannot start with a number
+# 1name = "Shakib"       # Invalid
+
+
+# Rule 3: Can contain letters, numbers, and underscores
+student1 = "Shakib"
+student_1 = "Shakib"
+
+
+# Rule 4: Variable names are case-sensitive
+name = "Shadman"
+Name = "Shakib"
+
+print(name)
+print(Name)
+
+
+# Rule 5: Spaces are not allowed
+# student name = "Shakib"    # Invalid
+
+# Use underscore instead
+student_name = "Shakib"
+
+
+# Rule 6: Python keywords cannot be used
+# class = "CST"               # Invalid
+# if = 10                     # Invalid
+
+
+# Rule 7: Use descriptive and meaningful names
+age = 18
+student_name = "Shakib"
+student_age = 18
+
+
+# Rule 8: Use snake_case for multiple words
+first_name = "Shadman"
+last_name = "Shakib"
+student_id = 101
+
+
+# Valid Variable Names
+my_name = "Shadman"
+age = 18
+student2 = "Fahim"
+_student = "Fahad"
+
+
+# Invalid Variable Names
+# 2student = "Nirob"
+# my-name = "Shadman"
+# my name = "Shakib"
+# class = "CST"
