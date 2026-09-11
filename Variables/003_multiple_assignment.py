838bd8f4b6e8b978a87138b8c17a9ef5dc050c87
Shadman Shakib <shadmanshakib529@gmail.com>
2026-09-12 02:04:10 +0600

    Add files via upload

---
+++ Variables/003_multiple_assignment.py
@@ -0,0 +1,45 @@
+# Python Multiple Assignment
+#
+# Multiple Assignment:
+# Assign values to multiple variables in a single line.
+
+
+# 1. Assign different values to multiple variables
+name, age, country = "Oriyo", 3, "Bangladesh"
+
+print(name)
+print(age)
+print(country)
+
+
+# 2. Assign multiple numbers
+x, y, z = 10, 20, 30
+
+print(x)
+print(y)
+print(z)
+
+
+# 3. Assign the same value to multiple variables
+a = b = c = 100
+
+print(a)
+print(b)
+print(c)
+
+
+# 4. Multiple assignment with different data types
+text, number, is_valid = "Python", 10, True
+
+print(text)
+print(number)
+print(is_valid)
+
+
+# 5. Multiple assignment using existing variables
+first = "Hello"
+second = "World"
+
+message = first + " " + second
+
+print(message)
