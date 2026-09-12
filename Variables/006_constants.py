c83881e7ab8af17a9d78329a43d57bf100112ef9
Shadman Shakib <shadmanshakib529@gmail.com>
2026-09-12 21:05:50 +0600

    Add constants

---
+++ Variables/006_constants.py
@@ -0,0 +1,19 @@
+# Python Constants
+
+
+# Constants are usually written in uppercase
+PI = 3.14159
+MAX_VALUE = 100
+MIN_VALUE = 0
+
+print(PI)
+print(MAX_VALUE)
+print(MIN_VALUE)
+
+
+# Constant with a string
+APP_NAME = "Python"
+VERSION = "2.69"
+
+print(APP_NAME)
+print(VERSION)
