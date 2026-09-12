b54c1b81940d4821a7b0694206248fef06193a2a
Shadman Shakib <shadmanshakib529@gmail.com>
2026-09-12 21:05:49 +0600

    Add variable reassignment

---
+++ Variables/004_variable_reassignment.py
@@ -0,0 +1,27 @@
+# Python Variable Reassignment
+
+
+# Reassigning a variable
+x = 10
+print(x)
+
+x = 20
+print(x)
+
+
+# Reassigning with a different data type
+value = 100
+print(value)
+
+value = "Python"
+print(value)
+
+
+# Reassigning using another variable
+a = 10
+b = 20
+
+a = b
+
+print(a)
+print(b)
