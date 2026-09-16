719b61b339d220616e8c439571136fdf88445c01
Shadman Shakib <shadmanshakib529@gmail.com>
2026-09-16 12:01:27 +0600

    Add function basics

---
+++ functions/001_function_basics.py
@@ -0,0 +1,32 @@
+# Python Function Basics
+
+"""
+ A function is a block of reusable code that performs a specific task.
+"""
+
+# Syntax:
+# def function_name():
+#     statement
+#
+# function_name()
+
+
+#   Creating a simple function
+def greet():
+    print("Hello, Python!")
+greet()     # Calling the function
+
+
+#   Function with multiple statements
+def welcome():
+    print("Welcome to Python")
+    print("Let's learn functions")
+welcome()
+
+
+#   3. Calling a function multiple times
+def message():
+    print("Learning Python")
+message()
+message()
+message()
