072bf8ac28b6d8264b4fe6b26dc41e2fe9ac8ebe
Shadman Shakib <shadmanshakib529@gmail.com>
2026-09-21 19:30:30 +0600

    Add module alias example

---
+++ modules/003_module_alias.py
@@ -0,0 +1,12 @@
+# Python Module Alias
+#
+# An alias gives a module a shorter or different name.
+#
+# Syntax:
+# import module_name as alias
+
+
+import math as m
+
+print(m.sqrt(49))
+print(m.pi)
