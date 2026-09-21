dbb67c4102ec15a56113705b3daea85cf5bdb306
Shadman Shakib <shadmanshakib529@gmail.com>
2026-09-21 19:30:30 +0600

    Add import module example

---
+++ modules/001_import_module.py
@@ -0,0 +1,14 @@
+# Python Import Module
+#
+# A module is a Python file containing reusable code.
+#
+# Syntax:
+# import module_name
+#
+# module_name.function_name()
+
+
+import math
+
+print(math.sqrt(25))
+print(math.pi)
