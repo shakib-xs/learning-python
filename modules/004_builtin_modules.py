193248c9f74d04165cdcdf54a865ca374c601302
Shadman Shakib <shadmanshakib529@gmail.com>
2026-09-21 19:30:30 +0600

    Add builtin modules example

---
+++ modules/004_builtin_modules.py
@@ -0,0 +1,18 @@
+# Python Built-in Modules
+#
+# Python provides many built-in modules for common tasks.
+#
+# Examples:
+# math, random, datetime
+#
+# Syntax:
+# import module_name
+
+
+import math
+import random
+from datetime import datetime
+
+print("Square root:", math.sqrt(64))
+print("Random number:", random.randint(1, 10))
+print("Current date and time:", datetime.now())
