29859c08329650b42aaed7085d868044bd42e2a7
Shadman Shakib <shadmanshakib529@gmail.com>
2026-09-21 19:30:30 +0600

    Add from import example

---
+++ modules/002_from_import.py
@@ -0,0 +1,13 @@
+# Python From Import
+#
+# The from ... import statement imports specific
+# functions, classes, or variables from a module.
+#
+# Syntax:
+# from module_name import item
+
+
+from math import sqrt, pi
+
+print(sqrt(36))
+print(pi)
