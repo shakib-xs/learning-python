013f1fa05bdc1f09539fc6b6a287eff1ace96223
Shadman Shakib <shadmanshakib529@gmail.com>
2026-09-21 19:30:31 +0600

    Add custom module example

---
+++ modules/005_custom_module.py
@@ -0,0 +1,23 @@
+# Python Custom Module
+#
+# A custom module is a Python file created by the programmer.
+#
+# Syntax:
+# import module_name
+# module_name.function_name()
+
+
+# Example:
+# Suppose another file named calculator.py contains:
+#
+# def add(a, b):
+#     return a + b
+#
+# Then we can import it like this:
+#
+# import calculator
+# print(calculator.add(10, 20))
+
+
+# This file demonstrates the syntax.
+# Create calculator.py in the same folder to run the example.
